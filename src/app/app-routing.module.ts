3493cf8b94dc6569def6fa4803ca1f114297ab24
Gabriel Falcones <gafalcon@espol.edu.ec>
2019-10-07 18:39:02 -0400

    created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: SearchComponent },
+    { path: 'restaurant/:restaurant', component: RestaurantComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
